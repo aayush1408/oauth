ee8a3c674e870d4061153cf294f4265bbf34bdbe
Aayush <aayushjaiswal0114@gmail.com>
2017-11-04 16:02:49 +0530

    searialize and desearialize

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,18 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys.js');
 const User = require('../models/user.js');
+
+passport.serializeUser((user,done)=>{
+        done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+        User.findById(id).then((user)=>{
+        done(null,user);
+
+        })
+});
+
 passport.use(
              new GoogleStrategy({
                 callbackURL:'/auth/google/redirect',
@@ -12,6 +24,7 @@ passport.use(
 User.findOne({googleid:profile.id}).then((currentUser)=>{
 if(currentUser){
 console.log('currentUser');
+done(null,currentUser);
 }
 else{
 new User({
@@ -19,6 +32,7 @@ new User({
     googleid:profile.id
 }).save().then((newUser)=>{
     console.log(newUser);
+    done(null,newUser);
 });
 }
 });
